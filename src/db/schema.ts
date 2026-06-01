bd8a4392f8fad18264ed9f926a87fab2d712eaa4
boboluya <boboluya@gmail.com>
2026-06-01 21:54:28 +0800

    create user module

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,38 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  bigint,
+  bigserial,
+  integer,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users2", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const sys_usersTable = pgTable("sys_user", {
+  user_id: bigserial({ mode: "number" }),
+  dept_id: integer(),
+  user_name: varchar({ length: 20 }).notNull(),
+  nick_name: varchar({ length: 20 }).notNull(),
+  email: varchar({ length: 50 }).unique(),
+  phone_number: varchar({ length: 20 }),
+  sex: varchar({ length: 1 }),
+  avatar: varchar({ length: 255 }),
+  password: varchar({ length: 255 }).notNull(),
+  status: varchar({ length: 1 }),
+  del_flag: varchar({ length: 1 }),
+  login_ip: varchar({ length: 50 }),
+  login_datetime: timestamp({ withTimezone: true }),
+  create_dept: integer().notNull(),
+  create_by: integer().notNull(),
+  create_time: timestamp({ withTimezone: true }).notNull(),
+  update_dept: integer().notNull(),
+  update_by: integer().notNull(),
+  update_time: timestamp({ withTimezone: true }).notNull(),
+  remark: varchar({ length: 255 }),
 });
