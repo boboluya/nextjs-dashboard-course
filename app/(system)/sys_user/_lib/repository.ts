c9b71ed6be1c5fb132caadbdc124456345565ffb
lulu <liu@liub.com>
2026-06-02 17:41:15 +0800

    Integrate shadcn and update dependencies.

--- app/(system)/sys_user/_lib/repository.ts
+++ app/(system)/sys_user/_lib/repository.ts
@@ -1,6 +1,7 @@
+"use server"
 import { db } from "@/src/index";
 import { sys_usersTable } from "@/src/db/schema";
-import { eq ,and} from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import { SysUser } from "@/app/lib/definitions";
 
 function dbMapping(user: typeof sys_usersTable.$inferSelect): SysUser {
@@ -35,9 +36,39 @@ export async function findUserById(id: number): Promise<SysUser[]> {
     .where(
       and(
         eq(sys_usersTable.user_id, id),
-        eq(sys_usersTable.del_flag, '0'),
-        eq(sys_usersTable.status, '0')
-      )
+        eq(sys_usersTable.del_flag, "0"),
+        eq(sys_usersTable.status, "0"),
+      ),
+    );
+  return result.map(dbMapping);
+}
+
+export async function selectUsers(sysUser: SysUser): Promise<SysUser[]> {
+  const conditions = [
+    sysUser.userId ? eq(sys_usersTable.user_id, sysUser.userId) : undefined,
+    sysUser.deptId ? eq(sys_usersTable.dept_id, sysUser.deptId) : undefined,
+    sysUser.userName
+      ? eq(sys_usersTable.user_name, sysUser.userName)
+      : undefined,
+    sysUser.nickName
+      ? eq(sys_usersTable.nick_name, sysUser.nickName)
+      : undefined,
+    sysUser.email ? eq(sys_usersTable.email, sysUser.email) : undefined,
+    sysUser.phoneNumber
+      ? eq(sys_usersTable.phone_number, sysUser.phoneNumber)
+      : undefined,
+    sysUser.sex ? eq(sys_usersTable.sex, sysUser.sex) : undefined,
+    sysUser.avatar ? eq(sys_usersTable.avatar, sysUser.avatar) : undefined,
+    sysUser.status ? eq(sys_usersTable.status, sysUser.status) : undefined,
+    sysUser.deletedFlag
+      ? eq(sys_usersTable.del_flag, sysUser.deletedFlag)
+      : undefined,
+  ];
+  const result = await db
+    .select()
+    .from(sys_usersTable)
+    .where(
+      conditions.length > 0 ? and(...conditions.filter(Boolean)) : undefined,
     );
   return result.map(dbMapping);
 }
---
+++ app/(system)/sys_user/page.tsx
@@ -0,0 +1,3 @@
+export default function SysUserPage() {
+  return <div>Sys User Page</div>;
+}
