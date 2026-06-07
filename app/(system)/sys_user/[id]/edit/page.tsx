10bb5735f23f5c832600a7374e14a9e5e89c81e3
lulu <liu@liub.com>
2026-06-07 18:10:15 +0800

    change

--- app/(system)/sys_user/[id]/edit/page.tsx
+++ app/(system)/sys_user/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 import { fetchUserById } from "../../_lib/actions";
 import { EditForm } from "../../_components/edit_form";
+import Breadcrumbs from "@/components/custome_ui/breadcrumbs";
 import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
@@ -13,6 +14,12 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 
   return (
     <div>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Users", href: "/sys_user" },
+          { label: "Edit User", href: `/sys_user/${id}/edit`, active: true },
+        ]}
+      />
       <EditForm user={users[0]} />
     </div>
   );
--- app/(system)/sys_user/create/page.tsx
+++ app/(system)/sys_user/create/page.tsx
@@ -1,9 +1,16 @@
 import { CreateForm } from "../_components/create_form";
+import Breadcrumbs from "@/components/custome_ui/breadcrumbs";
 
 export default async function Page() {
   return (
     <div>
-      <CreateForm/>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Users", href: "/sys_user" },
+          { label: "Create User", href: "/sys_user/create", active: true },
+        ]}
+      />
+      <CreateForm />
     </div>
   );
 }
