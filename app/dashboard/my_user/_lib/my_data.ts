689b6969a3e36f5aea45e4c6d7beed8cc8a1a5b0
lulu <liu@liub.com>
2026-06-08 10:31:49 +0800

    update

--- app/dashboard/my_user/_lib/my_data.ts
+++ app/dashboard/my_user/_lib/my_data.ts
@@ -1,12 +1,6 @@
 import "server-only";
-import postgres from "postgres";
 import { db } from "@/src/index";
-
-const db_url = process.env.POSTGRES_URL;
-
-if (!db_url) {
-  throw new Error("POSTGRES_URL environment variable is not set");
-}
+import { sql } from "drizzle-orm";
 
 export type User = {
   userId: number;
@@ -24,7 +18,7 @@ export async function getTenUsers(
   pageSize: number,
 ): Promise<User[]> {
   // await new Promise((resolve) => setTimeout(resolve, 1000));
-  const result = await db<User[]>`SELECT
+  const result = await db.execute(sql`SELECT
     user_id as "userId",
     user_name as "userName",
     nick_name as "nickName",
@@ -35,6 +29,6 @@ export async function getTenUsers(
     FROM sys_user
     WHERE nick_name LIKE ${`%${queryName}%`}
     ORDER BY create_time DESC
-    LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize}`;
-  return result;
+    LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize}`);
+  return result.rows as User[];
 }
--- app/lib/my_data.ts
+++ app/lib/my_data.ts
@@ -1,10 +1,6 @@
 
-const db_url = process.env.POSTGRES_URL;
 import { db } from "@/src/index";
-
-if (!db_url) {
-  throw new Error("POSTGRES_URL environment variable is not set");
-}
+import { sql } from "drizzle-orm";
 
 type User = {
   userId: number;
@@ -16,8 +12,8 @@ type User = {
 
 export async function getTenUsers(): Promise<User[]> {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const result = await db<User[]>`SELECT user_id as userId,
-  user_name as userName, nick_name as nickName,
-  create_by as createBy, update_by as updateBy FROM sys_user LIMIT 10`;
-  return result;
+  const result = await db.execute(sql<User[]>`SELECT user_id as "userId",
+  user_name as "userName", nick_name as "nickName",
+  create_by as "createBy", update_by as "updateBy" FROM sys_user LIMIT 10`);
+  return result.rows as User[];
 }
